dbbd00f23c2a81b91dc28316d364d5f748a504e6
Cuong Desktop <nguyenc72694@gmail.com>
2019-09-28 17:58:04 -0500

    added table and it is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,5 +17,21 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connection successful " + connection.threadId);
+    displayTable();
 });
 
+function displayTable () {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        
+        var listTable = new table({
+            head: ["Item ID", "Product Name", "Department Name", "Price", "Quantity"],
+            colWidths: [10, 30, 30, 10, 15]
+        }); 
+
+        for (var i = 0; i < res.length; i++) {
+            listTable.push([res[i].item_id, res[i].product_name, res[i].department_name, `$${res[i].price}`, res[i].stock_quantity]);
+        }
+        console.log(`\n\n${listTable.toString()}\n\n`);
+    });
+}
