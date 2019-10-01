988d018b7a6f84977f022a4578d20e399ca0b1e6
Cuong Desktop <nguyenc72694@gmail.com>
2019-09-30 21:59:26 -0500

    added readme file

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,8 +17,16 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connection successful " + connection.threadId);
-    managerActions();
+    consoleTable();
 });
+
+function consoleTable() {
+    connection.query("SELECT item_id, product_name, department, price, stock_quantity FROM products", function (error, data) {
+        if (error) throw error;
+        console.table("\n-----------------------" + "Products" + "------------------------", data);
+        managerActions();
+    })
+}
 
 function managerActions() {
     inquirer.prompt([{
@@ -44,24 +52,3 @@ function managerActions() {
     })
 }
 
-function displayTable() {
-    var table = new Table({
-        head: ["Item ID", "Product Name", "Department Name", "Price", "Quantity"],
-        colWidths: [10, 30, 30, 10, 15]
-    });
-    listItems();
-
-    function listItems() {
-        connection.query("SELECT * FROM products", function (err, res) {
-            if (err) throw err;
-
-            for (var i = 0; i < res.length; i++) {
-                table.push([res[i].item_id, res[i].product_name, res[i].department_name, `$${res[i].price}`, res[i].stock_quantity]);
-            }
-            console.log(table.toString() + "\n");
-            //figure this function out. Promises?? How do I get it to laod after.....
-        }).then(function () {
-            managerActions();
-        });
-    }
-}
